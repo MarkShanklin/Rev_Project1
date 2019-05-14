138fb64043103c0c98ca6211a4cde2c069f45a5a
Mark Shanklin <mark.shanklin2@gmail.com>
2019-05-14 10:16:14 -0400

    Project1 made 1 changes

--- 0429ProjectOne/src/main/java/com/ers/Employee.java
+++ 0429ProjectOne/src/main/java/com/ers/Employee.java
@@ -1,10 +1,70 @@
 package com.ers;
 
 public class Employee {
+	
 	int usedId;
 	String username;
 	String password;
 	String firstName;
 	String lastName;
 	
+	public Employee(int usedId, String username, String password, String firstName, String lastName) {
+		super();
+		this.usedId = usedId;
+		this.username = username;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public void login()
+	{
+		//TODO login implementation. may not go here?
+	}
+
+	public int getUsedId() {
+		return usedId;
+	}
+
+	public void setUsedId(int usedId) {
+		this.usedId = usedId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	@Override
+	public String toString() {
+		return "Employee [usedId=" + usedId + ", username=" + username + ", password=" + password + ", firstName="
+				+ firstName + ", lastName=" + lastName + "]";
+	}
 }
